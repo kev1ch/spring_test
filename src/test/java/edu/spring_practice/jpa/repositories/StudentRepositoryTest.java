74c2ced7c77fc7a320a7b90960d10a70989bada6
daniil <danila546324@gmail.com>
2025-02-22 12:15:10 -0500

    Student Repository Test added

--- src/test/java/edu/spring_practice/jpa/repositories/StudentRepositoryTest.java
+++ src/test/java/edu/spring_practice/jpa/repositories/StudentRepositoryTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 public class StudentRepositoryTest {
 
@@ -13,7 +18,15 @@ public class StudentRepositoryTest {
 
     @Test
     void saveStudentTest() {
-        // Student test_student = new Student();
+        Student test_student = new Student();
+        test_student.setName("James");
+        test_student.setGroup(1);
+        test_student.setGpa(3.5f);
+        studentRepository.save(test_student);
+        Optional<Student> optional_student = studentRepository.findById(test_student.getId());
+        assertTrue(optional_student.isPresent());
+        Student test_student_2 = optional_student.get();
+        assertEquals(test_student, test_student_2);
     }
 
 }
